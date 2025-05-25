fbfbf3c05993a041cadaafb4ac1e4a5f6e82f3ba
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2025-05-25 15:04:21 -0500

    added a react ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+//
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+
+      <main className="max-w-7xl mx-auto px-4 py-10">
+        <div className="bg-white rounded-lg shadow p-8">
+          <h2 className="text-xl font-semibold mb-4"> エッチ</h2>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
