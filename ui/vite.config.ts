9bed17b48c3cdbd5d64d667fda45210e94531e67
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2025-05-29 12:47:20 -0500

    made it a pwa in order to get it to download

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Ecchi Reader',
+      short_name: 'Ecchi',
+      start_url: '.',
+      display: 'standalone',
+      background_color: '#000000',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:8080',
