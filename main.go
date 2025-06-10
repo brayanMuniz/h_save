9cb6576c310c777528c9644f854b3b14e1fb99f1
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2025-06-10 14:30:54 -0500

    updated gitignore

--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 	"github.com/brayanMuniz/h_save/db"
 	"github.com/brayanMuniz/h_save/routes"
-	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"os"
 )
 
 func main() {
@@ -19,26 +17,7 @@ func main() {
 	defer database.Close()
 	fmt.Println("Database configured!")
 
-	fmt.Println("Getting .env file ...")
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	rootURL := os.Getenv("ROOT_URL")
-	if rootURL != "" {
-		fmt.Println("root url loaded")
-	}
-
-	csrftoken := os.Getenv("CSRF_TOKEN")
-	if csrftoken != "" {
-		fmt.Println("csrftoken loaded")
-	}
-
-	sessionid := os.Getenv("SESSION_ID")
-	if sessionid != "" {
-		fmt.Println("sessionid loaded")
-	}
+	rootURL := "https://nhentai.net"
 
 	r := routes.SetupRouter(database, rootURL)
 	if err := r.Run(":8080"); err != nil {
